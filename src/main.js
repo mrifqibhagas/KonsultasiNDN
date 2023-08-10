06e7c7134a276a73cbfa1330b756acfdf2eadb1a
mrifqibhagas <75106809+mrifqibhagas@users.noreply.github.com>
2023-08-10 09:55:20 +0700

    main.js

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ async function datapasien(evt) {
   interest.mustBeFresh = true; 
   interest.lifetime = 10000;
   interest.appParameters = encoder.encode(app); //melakukan encode packet ndn
-  #awaitinterest.updateParamsDigest();
+  //await interest.updateParamsDigest();
 
   const t0 = Date.now();
   const data = await endpoint.consume(interest);
