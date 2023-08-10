e9b051fa88b45716856b0f5638b98570374da69b
mrifqibhagas <75106809+mrifqibhagas@users.noreply.github.com>
2023-08-10 09:59:19 +0700

    main.js

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ async function datapasien(evt) {
   interest.name = prefix; //membuat const baru untuk dari fungsi interest dan name
   interest.mustBeFresh = true; 
   interest.lifetime = 10000;
-  interest.appParameters = encoder.encode(app); //melakukan encode packet ndn
+  await interest.appParameters = encoder.encode(app); //melakukan encode packet ndn
   //await interest.updateParamsDigest();
 
   const t0 = Date.now();
